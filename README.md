63eaf295c90c9ff477b78586e7a72ea73d535417
d954mas <d954mas@gmail.com>
2025-12-11 12:12:08 +0500

    add change font example

--- README.md
+++ README.md
@@ -60,4 +60,68 @@ You will have 6 files.
 
 //todo add image
 
-6. 
+6. Add localization_compact.json to custom resources
+
+7. Use localization in your game. Look in localization.lua and main.gui_script for example.
+
+```lua
+local I18N = require "localization.i18n.init"
+
+local TAG = "[LOCALIZATION]"
+
+---@class Localization
+local M = {
+
+}
+
+
+function M:init(locale)
+	self.config_all = localization.load_localization_from_resources("custom/localization_compact.json")
+	self.config_tiny = {}
+	for k, v in pairs(M.config_all) do
+		if k ~= "ko" and k ~= "zh" and k ~= "ja" then
+			self.config_tiny[k] = v
+		end
+	end
+	I18N.load(self.config_tiny)
+	I18N.setFallbackLocale("en")
+	self:set_locale(assert(locale))
+end
+
+function M:set_locale(locale)
+	print(TAG, "set locale:" .. locale)
+	I18N.setLocale(locale)
+    --you can create events or use messages to trigger components when language is changed
+    --EVENTS.LANGUAGE_CHANGED:trigger()
+end
+
+function M:locale_get()
+	return I18N.getLocale()
+end
+
+function M:translate(key, data)
+	local translation = I18N.translate(key, data)
+	if not translation then
+		print(TAG, "translation not found:" .. key)
+		return key
+	end
+	return translation
+end
+
+function M:is_exist(id)
+	return I18N.translate(id) ~= nil
+end
+
+function M:on_font_loaded(font_all)
+	self.font_all = assert(font_all)
+	I18N.load(self.config_all)
+    --add delay to fix some font not changed it metrics
+	--timer.delay(0.1, false, function ()
+	--	EVENTS.LANGUAGE_CHANGED:trigger()
+	--end)
+end
+
+return M
+
+```
+
--- localization/localization.lua
+++ localization/localization.lua
@@ -1,6 +1,6 @@
 local I18N = require "localization.i18n.init"
 
-local TAG = "LOCALIZATION"
+local TAG = "[LOCALIZATION]"
 
 ---@class Localization
 local M = {
@@ -9,7 +9,7 @@ local M = {
 
 
 function M:init(locale)
-	self.config_all = localization.load_localization_from_resources()
+	self.config_all = localization.load_localization_from_resources("/custom/localization_compact.json")
 	self.config_tiny = {}
 	for k, v in pairs(M.config_all) do
 		if k ~= "ko" and k ~= "zh" and k ~= "ja" then
@@ -17,15 +17,15 @@ function M:init(locale)
 		end
 	end
 	I18N.load(self.config_tiny)
-
-	--I18N.load(LOCALIZATION_LOCAL)
 	I18N.setFallbackLocale("en")
 	self:set_locale(assert(locale))
 end
 
 function M:set_locale(locale)
-	print("set locale:" .. locale, TAG)
+	print(TAG, "set locale:" .. locale)
 	I18N.setLocale(locale)
+    --you can create events or use messages to trigger components when language is changed
+    --EVENTS.LANGUAGE_CHANGED:trigger()
 end
 
 function M:locale_get()
@@ -35,7 +35,7 @@ end
 function M:translate(key, data)
 	local translation = I18N.translate(key, data)
 	if not translation then
-		print("translation not found:" .. key, TAG)
+		print(TAG, "translation not found:" .. key)
 		return key
 	end
 	return translation
@@ -48,6 +48,11 @@ end
 function M:on_font_loaded(font_all)
 	self.font_all = assert(font_all)
 	I18N.load(self.config_all)
+	pprint(self.config_all)
+    --add delay to fix some font not changed it metrics
+	--timer.delay(0.1, false, function ()
+	--	EVENTS.LANGUAGE_CHANGED:trigger()
+	--end)
 end
 
 return M
